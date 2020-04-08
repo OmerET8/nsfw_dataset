191a8649e43211eb55b8d7c89b48ef917e7e7b44
OmerET8 <omer.et8@gmail.com>
2020-04-09 01:23:12 +0300

    Update 5_create_train_.sh

--- scripts/5_create_train_.sh
+++ scripts/5_create_train_.sh
@@ -6,11 +6,8 @@ raw_data_dir="$base_dir/raw_data"
 data_dir="$base_dir/data"
 
 declare -a class_names=(
-	"neutral"
-	"drawings"
-	"sexy"
-	"porn"
-	"hentai"
+	"sfw"
+	"nsfw"
 	)
 
 train_dir="$data_dir/train"
